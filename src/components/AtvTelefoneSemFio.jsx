a0dea393c348ca663ee579ac1ca041b58dd4732f
bianca-beatriz-nave <bianca.beatriz@nave.org.br>
2026-07-03 16:17:37 -0300

    Update AtvTelefoneSemFio.jsx

--- src/components/AtvTelefoneSemFio.jsx
+++ src/components/AtvTelefoneSemFio.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 /*
 [ATIVIDADE TELEFONE SEM FIO]
 
@@ -7,3 +9,48 @@ Cada <button> ao ser clicado, deve inserir na <div> abaixo um <p> e um <input>,
 e substituir o conteúdo anterior dessa <div> de baixo dos botões.
 O texto digitado no <input> ativado por um <button> deve ser exibido no <p> do outro <button>, e vice-versa.
 */
+const style = {
+    viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+
+}
+export default function AtvTelefoneSemFio() {
+    const [texto1, setTexto1] = useState("")
+    const [texto2, setTexto2] = useState("")
+
+    const pagina1 = <div>
+                <p>{texto2}</p>
+                <input
+                type="text"
+                onChange={(e) => setTexto1(e.target.value)}
+                placeholder="Digite aqui o novo texto"
+                ></input>
+            </div>
+
+    const pagina2 = <div>
+                <p>{texto1}</p>
+                <input
+                type="text"
+                onChange={(e) => setTexto2(e.target.value)}
+                placeholder="Digite aqui o novo texto"
+                ></input>
+            </div>
+
+const [paginaAtual, setPaginaAtual] = useState(pagina1)
+
+    return(
+    <>
+        <div style={style.viewBotoes}>
+            <button onClick={() => setPaginaAtual(pagina1)}>Botão 1</button>
+            <button onClick={() => setPaginaAtual(pagina2)}>Botão 2 </button>
+        </div>
+        {paginaAtual}
+    </>    
+    )
+}
