9e4bf865f660d76159ab48f41e0f266c1261c04f
miguel-angelo-nave <miguel.angelo@nave.org.br>
2026-07-03 16:30:11 -0300

    Update AtvDobra.jsx

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -4,4 +4,16 @@
 Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argumento "numero". 
 O componente deve exibir uma <div> contendo um <p>, que vai exibir este número recebido como argumento, 
 e um <button>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
-*/
+*/import { useState } from "react"
+
+export default function AtvDobra({numero}){
+    const [valor, setValor] = useState(numero)
+    function Dobra(){setValor(valor*2)}    
+    return(
+        <div>
+            <p>{valor}</p>
+            <button onClick={() => Dobra()}>Dobra aqui</button>
+        </div>
+    )
+}
+
