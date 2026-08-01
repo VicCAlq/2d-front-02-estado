f7e0fb348b528cd072e1e474dd809417e5554c69
David-luduvico-nave <david.luduvico@nave.org.br>
2026-08-01 14:57:37 -0300

    ExParcialmenteFeitos

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,21 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <div> contendo um <p>, que vai exibir este número recebido como argumento, 
 e um <button>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+
+import {useState} from 'react'
+
+function AtvDobra({numero}) {
+
+    const [valor, setValor] = useState(numero);
+
+    return(
+
+        <div>
+            <p>{valor}</p>
+
+            <button onClick={() => setValor(valor * 2)}>Dobrar</button>
+        </div>
+    )
+}
+
+export default AtvDobra
--- src/components/AtvMudaConteudo.jsx
+++ src/components/AtvMudaConteudo.jsx
@@ -8,3 +8,55 @@ Ao clicar no segundo <button>, o conteúdo dessa <div> deve ser substituído por
 onde cada uma destas <divs> deve ter o tamanho de 200px de altura por 200px de largura, 
 a da esquerda deve ter a cor "purple", a do meio a cor "teal" e a da direita a cor "orange".
 */
+
+import {useState} from 'react'
+
+function AtvMudaConteudo() {
+
+    const[conteudo, setConteudo] = useState("");
+
+    return(
+
+        <div>
+            <button onClick={() => setConteudo("texto")}>Página</button>
+
+            <button onClick={() => setConteudo("quadrados")}>Seções</button>
+
+            <div>
+
+                {conteudo === "texto" && (
+                <p>Página em construção</p>
+            )}
+
+             {conteudo === "quadrados" && (
+
+                <div style={{display: "flex"}}>
+
+                    <div style={{
+                        width: "200px",
+                        height: "200px",
+                        backgroundColor: "purple"
+                    }}></div>
+
+                    <div style={{
+                        width: "200px",
+                        height: "200px",
+                        backgroundColor: "teal"
+                    }}></div>
+
+                    <div style={{
+                        width: "200px",
+                        height: "200px",
+                        backgroundColor: "orange"
+                    }}></div>
+                    
+                </div>
+            )}
+
+            </div>
+
+        </div>
+    )
+}
+
+export default AtvMudaConteudo
