05a4bc3fb33ca96217f216821bf08f952b283e30
eduardo-cristiano-nave <eduardo.cristiano@nave.org.br>
2026-08-03 14:36:45 -0300

    Update AtvMudaConteudo.jsx

--- src/components/AtvMudaConteudo.jsx
+++ src/components/AtvMudaConteudo.jsx
@@ -8,3 +8,59 @@ Ao clicar no segundo <button>, o conteúdo dessa <div> deve ser substituído por
 onde cada uma destas <divs> deve ter o tamanho de 200px de altura por 200px de largura, 
 a da esquerda deve ter a cor "purple", a do meio a cor "teal" e a da direita a cor "orange".
 */
+import { useState } from "react";
+
+export default function AtvMudaConteudo() {
+  const [conteudo, setConteudo] = useState("");
+
+  function mostrarTexto() {
+    setConteudo("texto");
+  }
+
+  function mostrarCores() {
+    setConteudo("cores");
+  }
+
+  return (
+    <div>
+      {/* Botões */}
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button onClick={mostrarTexto}>Mostrar Texto</button>
+        <button onClick={mostrarCores}>Mostrar Cores</button>
+      </div>
+
+      {/* Conteúdo abaixo */}
+      <div style={{ marginTop: "20px" }}>
+        {conteudo === "texto" && <p>Página em construção</p>}
+
+        {conteudo === "cores" && (
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div
+              style={{
+                width: "200px",
+                height: "200px",
+                backgroundColor: "purple",
+              }}
+            ></div>
+
+            <div
+              style={{
+                width: "200px",
+                height: "200px",
+                backgroundColor: "teal",
+              }}
+            ></div>
+
+            <div
+              style={{
+                width: "200px",
+                height: "200px",
+                backgroundColor: "orange",
+              }}
+            ></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
